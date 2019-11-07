5bef06c7a0b34c1c88376029b3ed2ad739ad777f
Antonio <antoniomartinov1998@gmail.com>
2019-11-07 17:47:20 +0000

    includes

--- Sobel.cpp
+++ Sobel.cpp
@@ -1,10 +1,5 @@
 #include <stdio.h>
-#include <opencv2/highgui.hpp>
-#include <opencv2/opencv.hpp>
-#include <opencv2/highgui/highgui_c.h>
-
-#include <opencv2/imgproc.hpp>
-#include <opencv2/imgproc/imgproc_c.h>
+#include "includes.h"
 
 #include <math.h>     
 #define _USE_MATH_DEFINES
